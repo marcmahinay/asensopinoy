180d2120adff24d70466be5c96990333ba71d8df
marcmahinay <marc.mahinay@gmail.com>
2024-07-27 14:09:13 +0800

    Initialize controllers

---
+++ app/Http/Controllers/AyudaMemberController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AyudaMember;
+use Illuminate\Http\Request;
+
+class AyudaMemberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AyudaMember $ayudaMember)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AyudaMember $ayudaMember)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AyudaMember $ayudaMember)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AyudaMember $ayudaMember)
+    {
+        //
+    }
+}
